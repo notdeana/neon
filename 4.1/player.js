e6596050471c07ed352dece580c19958ff8239e6
おれわころせ <32080425+notdeana@users.noreply.github.com>
2021-05-20 22:24:06 -0700

    Add files via upload

--- 4.1/player.js
+++ 4.1/player.js
@@ -1,6 +1,6 @@
 class Player {
   constructor() {
-    this.r = 70;
+    this.r = 70; //diameter not radius
     this.x = w / 2;
     this.y = h - this.r;
     this.speed = 2;
@@ -8,7 +8,7 @@ class Player {
   }
   display() {
     image(playerImg, this.x, this.y, this.r, this.r);
-      //rect(this.x, this.y, this.r, this.r);
+    //rect(this.x, this.y, this.r, this.r);
   }
   move() {
 
@@ -17,30 +17,30 @@ class Player {
         //dont move anything
         break;
       case 'up':
-      if (this.y > 0){
-        //decrease y poss
-        //y--;
-        this.y -= this.speed;
-      }
+        if (this.y - this.r / 2 > 0) {
+          //decrease y poss
+          //y--;
+          this.y -= this.speed;
+        }
         break;
       case 'down':
-      if (this.y < h - this.r){
-      this.y += this.speed;
-    }
+        if (this.y < h - this.r / 2) {
+          this.y += this.speed;
+        }
         break;
       case 'right':
         //inc x pos
-        if (this.x < w - this.r){
-      this.x += this.speed;
-      }
+        if (this.x < w - this.r / 2) {
+          this.x += this.speed;
+        }
         break;
       case 'left':
-      if (this.x > 0){
-      this.x -= this.speed;
-    }
+        if (this.x - this.r / 2 > 0) {
+          this.x -= this.speed;
+        }
         //decreasing x pos
         break;
-        default:
+      default:
         break;
     }
   }
